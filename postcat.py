08dcca3f1fabcf8b867249f87ce572ee16e28bae
Antonio Varela Lizardi <antonio.varela@icon.mx>
2017-09-09 13:36:40 -0700

    Created script to cat postfix queue files scanning the name and using the proper directory.

---
+++ postcat.py
@@ -0,0 +1,41 @@
+# Postcat
+
+import os
+import argparse
+
+SPOOL_DIR = '/var/spool/postfix'
+POSTCAT_EXEC = '/usr/local/bin/postcat'
+QUEUE_DEFERRED = 'deferred'
+QUEUE_ACTIVE = 'active'
+
+queue_names = [QUEUE_ACTIVE, QUEUE_DEFERRED]
+
+
+def main(argv):
+    queue = QUEUE_DEFERRED
+    if argv.queue:
+        if argv.queue not in queue_names:
+            print "{} is not a valid queue name.".format(argv.queue)
+            exit(1)
+        else:
+            queue = argv.queue
+
+    # build the file's full path
+    queue_id = argv.queue_id
+    first = queue_id[0]
+    f = SPOOL_DIR + '/' + queue + '/' + first + '/' + queue_id
+    # build the cmd
+    cmd = "{} {}".format(POSTCAT_EXEC, f)
+
+    print os.popen(cmd).read()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Queue data browser', version='1.0')
+
+    parser.add_argument('queue_id', action='store', help='Name of the queue file')
+    parser.add_argument('--queue', action='store', help='Queue to browse. Valid names: deferred, active')
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ postcat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# postcat.py launcher, suitable for Linux/CentOS
+
+python_exe=/usr/bin/python
+postcat_py=postcat.py
+
+$python_exe $postcat_py $@
